4defa57c71246289fa6e57e23c2a1b2e0a1095d6
RyzeSeazudo <davibohnerbrisola@gmail.com>
2019-09-20 12:44:18 -0300

    Added utility function for URL redirecting, begun heavier Twig based content display, fixed dataflow editor sensors' item names

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,3 +1,8 @@
 function cleanString(str){
 	return str.trim().toLowerCase().split(" ").join("-").replace(/[^a-z0-9\-]+/gi, "");
 }
+
+function enterSublink(sublink){
+	let currentLoc = window.location.pathname;
+	window.location.replace(currentLoc + (currentLoc.endsWith("/") ? "" : "/") + sublink);
+}
